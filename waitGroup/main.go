d31a4f9119c6cb81c782936b9e8567bd2ae0eaeb
rishabh21g <biasedengineer@gmail.com>
2025-10-03 22:09:42 +0530

    wait group done

---
+++ waitGroup/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// WaitGroup is used to wait for a collection of goroutines to finish.
+// The main goroutine calls Add to set the number of goroutines to wait for.
+// Then each of the goroutines runs and calls Done when finished.
+// At the same time, Wait can be used to block until all goroutines have finished.
+var wg sync.WaitGroup
+
+func Worker(id int) {
+	defer wg.Done()
+	fmt.Println("Worker", id, "started")
+	time.Sleep(time.Second)
+	fmt.Println("Worker", id, "done")
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go Worker(i)
+	}
+	wg.Wait()
+	fmt.Println("Main code done")
+}
+
+// Output random but wait till last all go routine finishes it works
+
+// without wait group it will not wait for all go routines to finish
+
+// func Worker(id int) {
+
+// 	fmt.Println("Worker", id, "started")
+// 	time.Sleep(time.Second)
+// 	fmt.Println("Worker", id, "done")
+// }
+
+// func main() {
+// 	for i := 0; i < 5; i++ {
+
+// 		go Worker(i)
+// 	}
+
+// 	fmt.Println("Main code done")
+// }
